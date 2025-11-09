62b1e170c6268b5452727fc3a9dc4c78f66e732e
Péricles Júnior <periclesmjr@hotmail.com>
2025-11-09 17:31:54 +0000

    adiciona parágrafo

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Gatinha, eu te amo muito. Se você me ama, da uma risadinha rsrs! 😁❤️</h1>
+            <p>Semana que vem vou para Irecê 🙌</p>
         </div>
     );
 }
