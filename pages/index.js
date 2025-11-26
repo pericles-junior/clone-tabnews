4014043d4d14c56cc4f24d258420ce3999ea7481
Péricles Júnior <98140593+pericles-junior@users.noreply.github.com>
2025-11-26 22:45:34 +0000

    adiciona scripts `lint:check` e `lint:write`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 function Home() {
-    return (
-        <div>
-            <h1>Gatinha, eu te amo muito. Se você me ama, da uma risadinha rsrs! 😁❤️</h1>
-            <p>Semana que vem vou para Irecê 🙌</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>
+        Gatinha, eu te amo muito. Se você me ama, da uma risadinha rsrs! 😁❤️
+      </h1>
+      <p>Semana que vem vou para Irecê 🙌</p>
+    </div>
+  );
 }
 
 export default Home;
